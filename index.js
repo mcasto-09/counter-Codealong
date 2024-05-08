233c382b74e8668554abf7f93fbf849b842d9fb8
mcasto-09 <mgcastorani@gmail.com>
2024-05-07 21:09:36 -0600

    really cool counter

---
+++ index.js
@@ -0,0 +1,52 @@
+let count = 0;
+const counterContainer = document.querySelector('h1');
+console.log(counterContainer);
+
+function increase () {
+    count++;
+    counterContainer.innerText = count
+    console.log(count);
+}
+function decrease() {
+    count--;
+    counterContainer.innerText = count
+    console.log(count);
+}
+
+
+document.querySelector('#decrement').addEventListener('click', decrease);
+
+document.querySelector('#increment').addEventListener('click', increase);
+
+document.querySelector('#reset').addEventListener('click', () => {
+    count = 0
+    counterContainer.innerHTML = '<mark>0</mark>'
+    console.log(count);
+});
+
+
+// themes// 
+
+function setTheme(theme) {
+    document.querySelector('body').className = theme;
+    document.querySelector('main').className = theme;
+const buttons = document.querySelectorAll('button')
+console.log(buttons);
+
+buttons.forEach((button) => {
+    button.className = theme;
+});
+}
+
+const themeButtons = document.querySelectorAll('header button');
+
+console.log(themeButtons)
+
+themeButtons.forEach((buttonNode) => {
+    console.log(buttonNode);
+    buttonNode.addEventListener('click', (e) => {
+        console.log('click');
+        console.dir(e.target.innerText.toLowerCase());
+        setTheme(e.target.innerText.toLowerCase())
+    });
+});
